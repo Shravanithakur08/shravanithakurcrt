0c4945b93c293e6e1444b9f5dd4a8cd713ef6d56
Shravanithakur08 <thakurshravani31@gmail.com>
2024-07-02 15:41:33 +0530

    Create Strings.java

---
+++ Strings.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Strings
+{
+public static void main(String[]args)
+{
+Scanner s=new Scanner(System.in);
+String str="Java is simple language";
+System.out.println(str.contains("simple"));
+System.out.println(str.contains("python"));
+System.out.println(str.contains("lan"));
+System.out.println(str.contains("J"));
+System.out.println(str.contains("A"));
+System.out.println(str.contains("age"));
+}
+}
