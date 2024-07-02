62d375bf1c237fcafc1c4576745de15d5038584f
Shravanithakur08 <thakurshravani31@gmail.com>
2024-07-02 16:01:07 +0530

    Create SubString.java

---
+++ SubString.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class SubString
+{
+public static void main(String []args)
+{
+Scanner s=new Scanner(System.in);
+String str1="java is simple language";
+System.out.println(str.substring(12));
+System.out.println(str.substring(8,18));
+}
+}
